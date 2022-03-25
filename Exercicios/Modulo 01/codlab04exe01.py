a2f2bfdbdcce33ac5f8473b6438a04f9dbd8ce1e
Emerson Lima <elimacruz84@gmail.com>
2022-03-24 21:08:44 -0300

    remoção da pasta

--- Exercicios/Modulo 01/codlab04exe01.py
+++
@@ -1,9 +0,0 @@
-# Peça ao usuário uma palavra e a devolva com as letras ao contrário. Caso a palavra seja a mesma nos dois sentidos, imprima uma mensagem dizendo que é um palíndromo.
-palavra = input("Digite uma palavra: ").upper().strip()
-palavra_invertida = ""
-
-for letra in range(len(palavra)-1,-1,-1): # Pecorrer todas as letras começando pelo ultimo index e terminando em 0 de forma decrescente
-    palavra_invertida += palavra[letra]
-print(f"A palavra invertida é {palavra_invertida}")
-if palavra == palavra_invertida:
-    print("A palavra digitada é uma PALÍNDROMA")
--- Exercicios/Modulo 01/codlab04exe06.py
+++
@@ -1,7 +0,0 @@
-# Escreva um programa que liste todos os números de -100 a 100 (com os extremos incluídos) e que todo múltiplo de 3 e 4 seja substituído pela palavra Aqui.
-
-for x in range(-100,101):
-    if x % 3 == 0 and x % 4 == 0:
-        print("Aqui")
-    else:
-        print(x)
